a3b90771eed18a23c55d45bf499990c8bfb86c93
ueiheng <87272137+ueiheng@users.noreply.github.com>
2022-10-21 23:43:27 +0800

    Delete Desktop directory

--- Desktop/my_Blog/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/fonts/font.css'
-import 'element-plus/es/components/message/style/css'
-
-
-createApp(App).mount('#app')
-
